41717e7c2a995bec8c7888593768900366ae5e37
hatto1023 <s.hatto1023@gmail.com>
2023-09-25 23:24:07 +0900

    AtCoder Beginners Selection

---
+++ Product.py
@@ -0,0 +1,12 @@
+a,b = map(int, input().split())
+if a*b % 2 == 1:
+  print("Odd")
+else:
+  print("Even")
+
+# 解答例
+a, b = map(int, input().split())
+if (a*b) % 2:
+  print('Odd')
+else:
+  print('Even')
