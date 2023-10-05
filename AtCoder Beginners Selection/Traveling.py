d1938a69b73e631167e7c56d36bfe81e22fe2d7a
hatto1023 <s.hatto1023@gmail.com>
2023-10-05 23:43:14 +0900

    AtCoder Beginners Selection

---
+++ AtCoder Beginners Selection/Traveling.py
@@ -0,0 +1,20 @@
+N=int(input())
+TX=[]
+tx=[]
+for i in range(N):
+  tx=[int(x) for x in input().split()]
+  TX.append(tx)
+  tx=[]
+
+TX=np.array(TX)
+print(TX[0,N-1])
+
+# 解答例
+# コード
+n = int(input())
+for i in range(n):
+  t,x,y = map(int,input().split())
+  if (x + y) > t or (x + y + t) % 2:
+    print("No")
+    exit()
+print("Yes")
